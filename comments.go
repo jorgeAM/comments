46f4d0650ba0c1beefa5aa8cce87c93fd30c7479
Jorge L <jorge.alfmur@gmail.com>
2018-10-08 16:33:23 -0500

    creamos servidor

--- comments.go
+++ comments.go
@@ -3,8 +3,11 @@ package main
 import (
 	"flag"
 	"log"
+	"net/http"
 
 	"github.com/jorgeAM/comments/migration"
+	"github.com/jorgeAM/comments/routes"
+	"github.com/urfave/negroni"
 )
 
 func main() {
@@ -16,4 +19,20 @@ func main() {
 		migration.Migrate()
 		log.Println("Finalizó la migración")
 	}
+
+	// Inicia las rutas
+	router := routes.InitRoutes()
+
+	// Inicia los middlewares
+	n := negroni.Classic()
+	n.UseHandler(router)
+
+	// Inicia el Servidor
+	server := &http.Server{
+		Addr:    ":8000",
+		Handler: n,
+	}
+
+	log.Println("Servidor corriendo en http://localhost:8000")
+	log.Println(server.ListenAndServe())
 }
